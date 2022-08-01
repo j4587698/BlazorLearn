9631f7e7dfef1bce8d44b41eec259d470642ddb6
jx <jx@jvxiang.com>
2022-08-01 09:30:49 +0800

    登录系统

---
+++ BlazorLearn/Controller/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using BlazorLearn.Entity;
+using BlazorLearn.Pages;
+using Furion.DataEncryption;
+using Furion.DynamicApiController;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorLearn.Controller;
+
+public class LoginController: IDynamicApiController
+{
+    public async Task<object> Post([FromBody]LoginVo loginVo)
+    {
+        if (string.IsNullOrEmpty(loginVo.UserName))
+        {
+            return new { code = 50000, message = "用户名不能为空" };
+        }
+        if (string.IsNullOrEmpty(loginVo.Password))
+        {
+            return new { code = 50000, message = "密码不能为空" };
+        }
+
+        var password = MD5Encryption.Encrypt(loginVo.Password);
+        var user = await UserEntity.Where(x =>
+            x.UserName == loginVo.UserName && x.Password == password).Include(x => x.Role).FirstAsync();
+        if (user != null)
+        {
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName!));
+            identity.AddClaim(new Claim(ClaimTypes.Role, user.Role!.Name!));
+            await Furion.App.HttpContext.SignInAsync(new ClaimsPrincipal(identity), new AuthenticationProperties(){IsPersistent = true, ExpiresUtc = loginVo.RememberMe? DateTimeOffset.Now.AddDays(5): DateTimeOffset.Now.AddMinutes(30)});
+
+            return new { code = 20000, message = "登录成功" };
+        }
+        return new { code = 50000, message = "用户名或密码错误" };
+    }
+}
---
+++ BlazorLearn/Pages/Login.razor.cs
@@ -0,0 +1,104 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using BootstrapBlazor.Components;
+using Furion.ClayObject;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorLearn.Pages;
+
+public partial class Login
+{
+    private string Title { get; set; } = "登录";
+
+    [SupplyParameterFromQuery]
+    [Parameter]
+    public string? ReturnUrl { get; set; }
+
+    private LoginVo LoginVo { get; set; } = new LoginVo();
+
+    [Inject]
+    [NotNull]
+    private AjaxService? AjaxService { get; set; }
+    
+    [Inject]
+    [NotNull]
+    public MessageService? MessageService { get; set; }
+
+    private Task OnSignUp()
+    {
+        throw new NotImplementedException();
+    }
+
+    private Task OnForgotPassword()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task DoLogin()
+    {
+        if (string.IsNullOrEmpty(LoginVo.UserName))
+        {
+            await MessageService.Show(new MessageOption()
+            {
+                Color = Color.Danger,
+                Content = "用户名不能为空"
+            });
+            return;
+        }
+        
+        if (string.IsNullOrEmpty(LoginVo.Password))
+        {
+            await MessageService.Show(new MessageOption()
+            {
+                Color = Color.Danger,
+                Content = "密码不能为空"
+            });
+            return;
+        }
+        
+        var ajaxOption = new AjaxOption
+        {
+            Url = "/api/login",
+            Data = LoginVo
+        };
+        var str = await AjaxService.GetMessage(ajaxOption);
+        if (string.IsNullOrEmpty(str))
+        {
+            await MessageService.Show(new MessageOption()
+            {
+                Color = Color.Danger,
+                Content = "登录失败"
+            });
+        }
+        else
+        {
+            dynamic ret = Clay.Parse(str);
+            if (ret.code != 20000)
+            {
+                await MessageService.Show(new MessageOption()
+                {
+                    Color = Color.Danger,
+                    Content = ret.message
+                });
+            }
+            else
+            {
+                await MessageService.Show(new MessageOption()
+                {
+                    Color = Color.Success,
+                    Content = "登录成功"
+                });
+                ReturnUrl ??= "/";
+                await AjaxService.Goto(ReturnUrl);
+            }
+        }
+    }
+}
+
+public class LoginVo
+{
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
