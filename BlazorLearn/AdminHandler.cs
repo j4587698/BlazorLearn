78427c2726d030e35b976043dd3e75efd7c2e25d
jx <jx@jvxiang.com>
2022-08-04 10:35:59 +0800

    使用Furion进行权限认证

---
+++ BlazorLearn/AdminHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using BlazorLearn.Entity;
+using Furion.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using RouteData = Microsoft.AspNetCore.Components.RouteData;
+
+namespace BlazorLearn;
+
+public class AdminHandler : AppAuthorizeHandler
+{
+    public override Task<bool> PipelineAsync(AuthorizationHandlerContext context, DefaultHttpContext httpContext)
+    {
+        if (!int.TryParse(context.User.FindFirst(ClaimTypes.Role)?.Value, out var roleId))
+        {
+            return Task.FromResult(false);
+        }
+        if (context.Resource is RouteData routeData)
+        {
+            var routeAttr = routeData.PageType.CustomAttributes.FirstOrDefault(x =>
+                x.AttributeType == typeof(RouteAttribute));
+            if (routeAttr == null)
+            {
+                return Task.FromResult(true);
+            }
+            else
+            {
+                var url = routeAttr.ConstructorArguments[0].Value as string;
+                var permission = PermissionEntity
+                    .Where(x => x.Roles!.Any(y => y.Id == roleId) && x.Url == url).First();
+                if (permission != null)
+                {
+                    return Task.FromResult(true);
+                }
+            }
+        }
+        
+        return Task.FromResult(false);
+    }
+}
